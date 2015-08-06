3177387eccf0cb8fd676628408794649b915f60c
Jacob Wan <jacob@wildlemur.com>
2015-08-05 17:22:05 -0700

    WIP

--- js/builder.js
+++ js/builder.js
@@ -1,16 +1,38 @@
 $(function() {
 
     $('#lookup-customer').click(function() {
-        var customerId = $("#customer-id").text();
+        var customerId = $("#customer-id").val();
         console.log('customer id: ' + customerId);
         var customerData = getCustomerData(customerId);
         console.log(customerData);
         //debugger;
     });
 
+    addDemoData();
+
     function getCustomerData(customerId) {
         if (window['localStorage']) {
-            var customerData = window['localStorage'].getItem('customerData');
+            var storage = window['localStorage'];
+            var customerData = storage.getItem('customerData');
+            console.log(customerData);
+            if (!customerData) {
+                customerData = {};
+                customerData[customerId] = {};
+                storage.setItem('customerData', JSON.stringify(customerData));
+                return customerData[customerId];
+            }
+            customerData = JSON.parse(customerData);
+            if (customerData[customerId]) {
+                return customerData[customerId];
+            }
+            customerData[customerId] = {};
+            storage.setItem('customerData', JSON.stringify(customerData));
+            return customerData[customerId];
         }
+        return null;
+    }
+
+    function addDemoData() {
+
     }
 });
